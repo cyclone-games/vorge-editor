861f9ca715dfc2e68236bc945b50f52222e9f59e
Nick Dugger <email@nickdugger.com>
2018-07-02 00:20:32 -0500

    quark

--- application/windows/root/index.js
+++ application/windows/root/index.js
@@ -1,5 +1,12 @@
+const Component = require('quark/core/Component');
+const element = require('quark/core/element');
+
 const Window = require('../../core/Window');
 
 module.exports = new Window('root', app => {
-    console.log('window', app);
+    document.body.appendChild(new class extends Component {
+        render () {
+            return element('main', { style: { color: 'red' } }, `hello, ${ app.kind }`);
+        }
+    })
 });
